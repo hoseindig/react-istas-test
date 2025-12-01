6f51ac48258f4311d11cfbd48e200c7acbb0e487
hoseinx <hoseinx@gmail.com>
2025-12-01 20:44:11 +0330

    rtl

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
 import "./index.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Navigation } from "./components/Navigation";
 import { HomePage } from "./pages/HomePage";
 import { UserTablePage } from "./pages/UserTablePage";
@@ -14,24 +14,43 @@ import { AdminTablePage } from "./pages/AdminTablePage";
 
 import { NotFoundPage } from "./pages/NotFoundPage";
 
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+
+const cacheRtl = createCache({
+  key: "mui-rtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+const theme = createTheme({
+  direction: "rtl",
+});
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Box sx={{ minHeight: "100vh", bgcolor: "#f5f5f5" }}>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/users" element={<UserTablePage />} />
-          <Route path="/sellers" element={<SellerTablePage />} />
-          <Route path="/admins" element={<AdminTablePage />} />
-          {/* <Route path="/users/add" element={<AddUserPage />} /> */}
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Box sx={{ minHeight: "100vh", bgcolor: "#f5f5f5" }}>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/users" element={<UserTablePage />} />
+              <Route path="/sellers" element={<SellerTablePage />} />
+              <Route path="/admins" element={<AdminTablePage />} />
+              {/* <Route path="/users/add" element={<AddUserPage />} /> */}
 
-          {/* Reusable Add Page */}
-          <Route path="/:step/add" element={<AddUserPage />} />
+              {/* Reusable Add Page */}
+              <Route path="/:step/add" element={<AddUserPage />} />
 
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Box>
-    </BrowserRouter>
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Box>
+        </BrowserRouter>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 // ============================================
 // FILE: src/main.tsx
 // ============================================
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const theme = createTheme({
+  direction: "rtl",
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
